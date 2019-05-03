2fbf8d46d48ecb2e1b388ce25b981898bee1b68d
Luna <f.luna@campus.fct.unl.pt>
2019-05-03 21:59:12 +0100

    db test script

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    user_name varchar (50) NOT NULL
+);
+
+INSERT INTO users (user_name) 
+VALUES 
+(
+  'filipe', 
+  'rafael', 
+  'ricardo'
+); 
