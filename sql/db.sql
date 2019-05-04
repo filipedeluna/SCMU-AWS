08994cdee18a2ed248fe1293e8f3747f4e6aed1f
Luna <f.luna@campus.fct.unl.pt>
2019-05-04 22:04:09 +0100

    db update

--- sql/db.sql
+++ sql/db.sql
@@ -9,9 +9,9 @@ DROP TABLE events;
 -- CREATE TABLES
 CREATE TABLE users (
     user_id       SERIAL PRIMARY KEY,
-    user_email    TEXT UNIQUE NOT NULL
-    user_name     TEXT NOT NULL
-    user_birthday DATE NOT NULL
+    user_email    TEXT UNIQUE NOT NULL,
+    user_name     TEXT NOT NULL,
+    user_birthday DATE NOT NULL,
     user_picture  TEXT NOT NULL
 );
 
@@ -23,26 +23,26 @@ CREATE TABLE cards (
 CREATE TABLE staff (
     staff_id       INTEGER PRIMARY KEY,
     staff_email    TEXT NOT NULL,
-    staff_name     TEXT NOT NULL
-    staff_password TEXT DEFAULT '123456' NOT NULL
+    staff_name     TEXT NOT NULL,
+    staff_password TEXT DEFAULT '123456' NOT NULL,
     staff_type     INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE entries (
     entry_id    SERIAL PRIMARY KEY,
-    card_id_ref INTEGER NOT NULL REFERENCES cards(card_id)
-    entry_date  DATE NOT NULL DEFAULT CURRENT_DATE
+    card_id_ref INTEGER NOT NULL REFERENCES cards(card_id),
+    entry_date  DATE NOT NULL DEFAULT CURRENT_DATE,
     entry_valid BOOLEAN NOT NULL DEFAULT TRUE
 
 );
 
 CREATE TABLE events (
     event_id          SERIAL PRIMARY KEY,
-    event_name        TEXT NOT NULL
-    event_description TEXT NOT NULL
-    event_date        DATE NOT NULL
-    event_tickets     INTEGER NOT NULL
-    event_price       NUMERIC NOT NULL
+    event_name        TEXT NOT NULL,
+    event_description TEXT NOT NULL,
+    event_date        DATE NOT NULL,
+    event_tickets     INTEGER NOT NULL,
+    event_price       NUMERIC NOT NULL,
     event_min_age     INTEGER NOT NULL
 );
 
@@ -50,7 +50,7 @@ CREATE TABLE tickets (
     ticket_id    SERIAL NOT NULL,
     card_id_ref  INTEGER REFERENCES cards(card_id),
     event_id_ref INTEGER REFERENCES users(user_id),
-    used         BOOLEAN NOT NULL DEFAULT FALSE
+    used         BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (ticket_id, card_id_ref, event_id_ref)
 );
 
